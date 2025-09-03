8a81ec54b3e59bbeb49bc27b10d15e961f3cfba1
pontinho-max <kaicki775@gmail.com>
2025-09-03 19:48:43 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const form = document.getElementById("composerForm");
 const input = document.getElementById("input");
 const tpl = document.getElementById("msgTemplate");
 
-const API_URL = "https://SEU_PROJETO.vercel.app/api/chat"; // ajuste aqui com o link do Vercel
+const API_URL = "https://mim-eight.vercel.app/api/chat"; // ajuste aqui com o link do Vercel
 
 function renderMessage(role, text) {
   const node = tpl.content.cloneNode(true);
